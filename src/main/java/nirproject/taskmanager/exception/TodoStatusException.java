0a4c903ed5b7b2e980b067740aefa04aa88b9501
krivonosovti <111201189+krivonosovti@users.noreply.github.com>
2023-12-18 04:19:13 +0300

    Create TodoStatusException.java

---
+++ src/main/java/nirproject/taskmanager/exception/TodoStatusException.java
@@ -0,0 +1,8 @@
+package nirproject.taskmanager.exception;
+
+public class TodoStatusException extends Exception{
+    public TodoStatusException (String message)
+    {
+        super(message);
+    }
+}
